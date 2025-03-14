dfb48e681bf168d08bab75c5b31db7957d8523a5
Franklys Guimarães <42286289+FranklysG@users.noreply.github.com>
2025-03-14 16:52:52 -0300

    Update check.js

--- js/check.js
+++ js/check.js
@@ -1,15 +1,15 @@
-// document.addEventListener('DOMContentLoaded', () => {
-//   const input = document.querySelector('input[name=jwt]');
+document.addEventListener('DOMContentLoaded', () => {
+  const input = document.querySelector('input[name=jwt]');
 
-//   if (input) {
-//     const observer = new MutationObserver(() => {
-//       if (input.value) {
-//         const baseUrl = document.body.getAttribute('data-base-url');
-//         localStorage.setItem('jwt', input.value);
-//         localStorage.setItem('base_url', baseUrl);
-//       }
-//     });
+  if (input) {
+    const observer = new MutationObserver(() => {
+      if (input.value) {
+        const baseUrl = document.body.getAttribute('data-base-url');
+        localStorage.setItem('jwt', input.value);
+        localStorage.setItem('base_url', baseUrl);
+      }
+    });
 
-//     observer.observe(input, { attributes: true, attributeFilter: ['value'] });
-//   }
-// });
+    observer.observe(input, { attributes: true, attributeFilter: ['value'] });
+  }
+});
